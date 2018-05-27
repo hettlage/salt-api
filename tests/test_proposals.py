bf5735890858b9a8fe234cf0b6577e6cd779b849
Christian Hettlage <hettlage@saao.ac.za>
2018-05-27 21:42:51 +0200

    Added tests

--- tests/test_proposals.py
+++ tests/test_proposals.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock
 from io import StringIO
 import re
 import zipfile
+import pytest
 import salt_api.proposals
 from salt_api.proposals import submit, zip_proposal_content
 
@@ -46,7 +47,16 @@ def test_zip_file_is_submitted_by_post_as_is(tmpdir, monkeypatch, dummy_zip):
     with open(zip_path, 'rb') as zip:
         submit(zip)
 
+        # a file is submitted...
         assert mock_post.call_args[1]['files']['file']
+
+        # ... and it has the correct content.
+        submitted_file = mock_post.call_args[1]['files']['file']
+        submitted_file.seek(0)
+        submitted_file_content = submitted_file.read()
+        zip.seek(0)
+        expected_file_content = zip.read()
+        assert submitted_file_content == expected_file_content
 
 
 def test_zip_proposal_content_includes_referenced_files(tmpdir):
@@ -102,16 +112,129 @@ def test_zip_proposal_content_includes_referenced_files(tmpdir):
     with zipfile.ZipFile(zip_filename, 'r') as z:
         names = z.namelist()
 
-        assert len(names) == 4
+        # XNL file and 3 attachments
         assert 'Proposal.xml' in names
+        assert len(names) == 4
 
+        # paths were updated correctly
         xml_content = z.read('Proposal.xml').decode()
         for m in re.finditer(r'Path>\s*([^<]*)\s*</[^>]+Path', xml_content, flags=re.MULTILINE):
             path = m.group(1).strip()
-            print('===' + path, m.group())
             assert path in names or path in ('automatic', 'auto-generated')
 
+            # content of attachment is correct
+            real_path = None
+            if path.endswith('.pdf'):
+                real_path = path1.strpath
+            elif path.endswith('.png'):
+                real_path = path2.strpath
+            elif path.endswith('.xlsx'):
+                real_path = path3.strpath
+            else:
+                continue  # the path is "automatic" or "auto-generated"
+            with open(real_path, 'rb') as f:
+                assert z.read(path) == f.read()
 
 
+def test_zip_proposal_content_takes_xml_dir_as_root_dir(tmpdir):
+    """zip_proposal_content takes the XML file's parent directory as the default for the attachment directory."""
+
+    xml_template = '''<?xml version="1.0"?>
+
+<Proposal>
+    <ns1:Path xmlns:ns1="http://www.saao.ac.za/ns1">{path}</ns1:Path>
+</Proposal>'''
+
+    # referenced file
+    attachment_path = tmpdir.join('a.pdf')
+    with open(attachment_path.strpath, 'w') as f:
+        f.write('This is a fake pdf file.')
+
+    # create the proposal content xml file
+    xml_file = tmpdir.join('proposal.xml')
+    with open(xml_file.strpath, 'w') as f:
+        f.write(xml_template.format(path='a.pdf'))
+
+    # ... and the corresponding zip file
+    zip_filename = tmpdir.join('proposal_content.zip')
+    zip_proposal_content(zip_filename, xml_file.strpath)
+
+    # check the zip file content
+    with zipfile.ZipFile(zip_filename, 'r') as z:
+        names = z.namelist()
+
+        # XML file and attachment
+        assert len(names) == 2
+        assert 'Proposal.xml' in names
+
+        # path was updated correctly
+        xml_content = z.read('Proposal.xml').decode()
+        m = re.search(r'Path>\s*([^<]*)\s*</[^>]+Path', xml_content, flags=re.MULTILINE)
+        path = m.group(1).strip()
+        assert path in z.namelist()
+
+        # content of attachment is correct
+        with open(attachment_path.strpath, 'rb') as f:
+            assert z.read(path) == f.read()
 
 
+def test_zip_proposal_content_uses_attachments_dir(tmpdir):
+    """zip_proposal_content uses the attachments directory passed to it."""
+
+    xml_template = '''<?xml version="1.0"?>
+
+<Proposal>
+    <ns1:Path xmlns:ns1="http://www.saao.ac.za/ns1">{path}</ns1:Path>
+</Proposal>'''
+
+    # referenced file
+    dir1 = tmpdir.mkdir('dir1')
+    attachment_path = dir1.join('a.pdf')
+    with open(attachment_path.strpath, 'w') as f:
+        f.write('This is a fake pdf file.')
+
+    # create the proposal content xml file
+    dir2 = tmpdir.mkdir('dir2')
+    xml_file = dir2.join('proposal.xml')
+    with open(xml_file.strpath, 'w') as f:
+        f.write(xml_template.format(path='a.pdf'))
+
+    # ... and the corresponding zip file
+    zip_filename = tmpdir.join('proposal_content.zip')
+    zip_proposal_content(zip_filename, xml_file.strpath, dir1.strpath)
+
+    # check the zip file content
+    with zipfile.ZipFile(zip_filename, 'r') as z:
+        names = z.namelist()
+
+        # XML file and attachment
+        assert len(names) == 2
+        assert 'Proposal.xml' in names
+
+        # path was updated correctly
+        xml_content = z.read('Proposal.xml').decode()
+        m = re.search(r'Path>\s*([^<]*)\s*</[^>]+Path', xml_content, flags=re.MULTILINE)
+        path = m.group(1).strip()
+        assert path in z.namelist()
+
+        # content of attachment is correct
+        with open(attachment_path.strpath, 'rb') as f:
+            assert z.read(path) == f.read()
+
+
+def test_zip_proposal_content_must_be_able_to_resolve_relative_paths(tmpdir):
+    """zip_proposal_content must raise a meaningful exception if the XML contains a relative file path, the value passed for the
+    xml parameter is not a string, and no value is passed for the attachments_dir parameter."""
+
+    xml = '''<?xml version="1.0"?>
+
+<Proposal>
+    <ns1:Path xmlns:ns1="http://www.saao.ac.za/ns1">a/relative/path</ns1:Path>
+</Proposal>'''
+
+    zip_filename = tmpdir.join('proposal_content.zip')
+
+    with pytest.raises(Exception) as excinfo:
+        zip_proposal_content(zip_filename, StringIO(xml))
+
+    assert 'no attachment directory' in str(excinfo.value)
